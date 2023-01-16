ff8ffe50382c22240b433eb71a56c38fd13a63f4
Andrés Botero <0xafbf@gmail.com>
2023-01-16 11:19:43 -0500

    Change path to variable

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -6,6 +6,7 @@ Push-Location $base_path
 $target_name = "ue427_template"
 
 $target_path = "$base_path/$target_name"
+
 mkdir "$target_path/Plugins"
 
 $plugin_remote_path = "git@github.com:vertexforge/DatasmithBlenderContent.git"
@@ -16,12 +17,10 @@ git clone $plugin_remote_path "$target_path/Plugins/DatasmithBlenderContent"
 
 $ue_path = "C:/Epic Games/UE_4.27"
 $ubt_path = "$ue_path/Engine/Binaries/DotNET/UnrealBuildTool.exe"
-& $ubt_path Development Win64 -Project="C:/abotero/vertexforge/templates/ue427_template/ue427_template.uproject" -TargetType=Editor -Progress -NoEngineChanges -NoHotReloadFromIDE
+$project_path = "$target_path/$target_name.uproject"
 
-# WARNING: Trying to build an enterprise target but the enterprise directory is missing. Falling back on engine components only.
-# Using 'git status' to determine working set for adaptive non-unity build (C:\abotero\vertexforge\templates).
-# Creating makefile for UE4Editor (no existing makefile)
 
+& $ubt_path Development Win64 "-Project=$project_path" -TargetType=Editor -Progress -NoEngineChanges -NoHotReloadFromIDE
 
 
 
