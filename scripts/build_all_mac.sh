e9ab9961a4b96c3ac3c4b2f24ada42572ed8bd7f
Andrés Botero <boterock@gmail.com>
2023-02-14 10:23:34 -0500

    fix error pendejo

--- scripts/build_all_mac.sh
+++ scripts/build_all_mac.sh
@@ -11,9 +11,6 @@ ue5_path="/Users/Shared/Epic Games/UE_5.1"
 
 pushd "$base_path"
 
-$ue4_path = "C:/Epic Games/UE_4.27"
-$ue5_path = "C:\Program Files\Epic Games\UE_5.1"
-
 scripts/build_single_mac.sh --target_name ue427_template --ue_path "$ue4_path"
 scripts/build_single_mac.sh --target_name ue51_template --ue_path "$ue5_path" --is_ue5
 
