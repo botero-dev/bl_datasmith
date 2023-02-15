905d8e8632baea377af614d10ace89a5467f533c
Andrés Botero <0xafbf@gmail.com>
2023-02-14 22:09:05 -0500

    match export paths

--- scripts/export_ue_all.ps1
+++ scripts/export_ue_all.ps1
@@ -5,7 +5,7 @@ $base_path = "$PSScriptRoot/.."
 
 Push-Location $base_path
 
-& scripts/export_ue_single.ps1 -template_path ue427_template -target_name "UE 4.27"
-& scripts/export_ue_single.ps1 -template_path ue51_template -target_name "UE 5.1"
+& scripts/export_ue_single.ps1 -template_path ue427_template -target_name "4.27"
+& scripts/export_ue_single.ps1 -template_path ue51_template -target_name "5.1"
 
 Pop-Location
