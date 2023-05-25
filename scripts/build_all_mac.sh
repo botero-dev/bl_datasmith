046fe7738fcc0682ffccb64b3b7399c4431907b2
Andrés Botero <boterock@gmail.com>
2023-05-24 23:23:03 -0500

    fix building plugin for mac

--- scripts/build_all_mac.sh
+++ scripts/build_all_mac.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# build_all_mac.sh
+# Copyright Andrés Botero 2023
 
 script_file="$0"
 script_dir=$(dirname "${BASH_SOURCE[0]}")
@@ -6,12 +8,16 @@ script_dir=$(dirname "${BASH_SOURCE[0]}")
 base_path="$script_dir/.."
 
 
+
 ue4_path="/Users/Shared/Epic Games/UE_4.27"
-ue5_path="/Users/Shared/Epic Games/UE_5.1"
+ue51_path="/Users/Shared/Epic Games/UE_5.1"
+ue52_path="/Users/Shared/Epic Games/UE_5.2"
 
 pushd "$base_path"
+base_path=$(pwd)
 
-scripts/build_single_mac.sh --target_name ue427_template --ue_path "$ue4_path"
-scripts/build_single_mac.sh --target_name ue51_template --ue_path "$ue5_path" --is_ue5
+scripts/build_single_mac.sh --target_path "$base_path/build/ue427" --ue_path "$ue4_path"
+#scripts/build_single_mac.sh --target_path "$base_path/build/ue51" --ue_path "$ue51_path"
+#scripts/build_single_mac.sh --target_path "$base_path/build/ue52" --ue_path "$ue52_path"
 
 popd
