a463c982b1bfc0cad70e63b0ce3804dd266736cf
Andrés Botero <0xafbf@gmail.com>
2023-11-12 19:35:56 -0500

    Script to use to remotely make a windows build

---
+++ scripts/windows-remote-build.ps1
@@ -0,0 +1,16 @@
+
+$build_path="C:/tmp/vertexforge_build"
+Remove-Item -Path $build_path -Recurse -Force
+New-Item -Path $build_path -ItemType Directory -Force
+
+git clone git@github.com:vertexforge/vertexforge.git $build_path
+Push-Location $build_path
+
+& scripts/build_all_win.ps1
+
+Compress-Archive -Path "build/win" -DestinationPath "build/win.zip"
+
+
+echo "FINISHED"
+
+Pop-Location
